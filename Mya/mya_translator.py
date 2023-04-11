a5c93dc250b49f03f5c208a7e5068dc80c143016
Chaerin Noh <sinye2002@korea.ac.kr>
2023-04-11 22:09:15 +0900

    mya

--- Mya/mya_translator.py
+++ Mya/mya_translator.py
@@ -4,11 +4,13 @@ import mya_decoder
 import sys
 # import re
 
-print("Please write the text...")
-text = sys.stdin.readline()
 
 print("To use incoder: Enter 0\nTo use decoder: Enter 1")
 flag = int(sys.stdin.readline())
+
+print("Please write the text...")
+text = sys.stdin.readline()
+
 print("[Result]")
 if flag:
     print(mya_decoder.myaDecoder(text))
