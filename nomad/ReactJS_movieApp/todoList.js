4e2043119098354ab6d7bf37f44a3f887cf06ca5
Chaerin Noh <sinye2002@korea.ac.kr>
2023-05-30 10:05:29 +0900

    todoList

---
+++ nomad/ReactJS_movieApp/todoList.js
@@ -0,0 +1,39 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [...currentArray, toDo]);
+    setToDo("");
+  };
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write what to do..."
+        ></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
